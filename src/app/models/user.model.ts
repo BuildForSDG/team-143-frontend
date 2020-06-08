b86ea98b01758a8fefee5cab77c8753c3311b626
tony254 <rubua31@gmail.com>
2020-06-08 18:10:49 +0300

    lands

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -10,20 +10,27 @@ export interface IUser {
     roles: [];
     email: string;
     accessToken: string;
-    address: {};
+    address: {
+        county: string;
+        mailbox: string;
+    };
 }
 
 export interface User {
-    id: number;
-    fullName:string;
-    idNumber: number;
-    lastSeenOn: string;
-    password: string;
-    phoneNumber: string;
-    pic: string;
-    _id: string;
-    roles: [];
-    email: string;
-    accessToken: string;
-    address: {};
+    id: number,
+    fullName:string,
+    idNumber: number,
+    lastSeenOn: string,
+    password: string,
+    phoneNumber: string,
+    pic: string,
+    _id: string,
+    roles: [],
+    email: string,
+    accessToken: string,
+    address: {
+        county: string,
+        mailbox: string,
+    },
+    blockChainKey: string
 }
--- src/app/users/land-registrar/dashboard/lr-dashboard.component.ts
+++ src/app/users/land-registrar/dashboard/lr-dashboard.component.ts
@@ -1,11 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { faUserPlus } from "@fortawesome/free-solid-svg-icons";
-import { faPlus } from "@fortawesome/free-solid-svg-icons";
-import { faExchangeAlt } from "@fortawesome/free-solid-svg-icons"; 
-import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { faCogs } from "@fortawesome/free-solid-svg-icons";
-import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faCalendarAlt } from "@fortawesome/free-solid-svg-icons";
+
 import { Globals } from "../../../../globals";
+import { User } from 'src/app/models/user.model';
 
 @Component({
   selector: 'app-lr-dashboard',
@@ -13,16 +10,37 @@ import { Globals } from "../../../../globals";
   styleUrls: ['./lr-dashboard.component.css']
 })
 export class LrDashboardComponent implements OnInit {
-  faUserPlus = faUserPlus;
-  faPlus = faPlus;
-  faExchangeAlt = faExchangeAlt;
-  faSearch = faSearch;
-  faCogs = faCogs;
-  faCheck = faCheck;
+  faCalendarAlt = faCalendarAlt;
+  
+  role;
+ 
 
   constructor(public globals: Globals) { }
 
   ngOnInit(): void {
+    this.getRoles();
+  
+  }
+
+  getRoles(){
+    let roles = this.globals.currentUser.roles;
+    
+    roles.forEach( (value,index,array) => {
+      if(array[value] === "1"){
+        return 'Administrator'
+        
+      }
+      if(array[value] === "2"){
+        return 'Land Registrar'
+      }
+      if(array[value] === "3"){
+        return 'Land Revenue Agent'
+      }
+      if(array[value] === "4"){
+        return 'Land Owner'
+      }
+    });
+
   }
 
 }
